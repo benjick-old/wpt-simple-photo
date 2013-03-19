2c8904b76105142e847987d75b95c2f9e3ebefb1
Max Malm <max@malm.me>
2013-03-19 14:33:02 +0100

    quickie

--- taxonomy-portfolio_cat.php
+++ taxonomy-portfolio_cat.php
@@ -24,8 +24,14 @@
 
 		<!--BEGIN: Archive-->
 		<article <?php post_class('post') ?> id="post-<?php the_ID(); ?>">
-		
-			<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('portfolio-feat'); ?><?php the_title(); ?></a></h2>
+        <div class="portfolioimage">
+            <?php the_post_thumbnail('portfolio-feat'); ?>
+            <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <div class="portfoliotext">
+                <?php the_content(); ?>
+            </div>
+        </div>
+
 
 			
 		</article>
